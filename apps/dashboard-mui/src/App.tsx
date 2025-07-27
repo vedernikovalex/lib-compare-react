4309fab50fba33be3c44af3ebb8d717f09eddd54
Alexander Vedernikov <alexander.vedernikov@viableone.com>
2025-07-27 11:39:18 +0200

    MUI-1: Initial application structure

---
+++ apps/dashboard-mui/src/App.tsx
@@ -0,0 +1,11 @@
+import { Container, Typography } from "@mui/material";
+
+export default function App() {
+  return (
+    <Container maxWidth="lg" sx={{ py: 4 }}>
+      <Typography variant="h4" gutterBottom>
+        Demo MUI Dashboard
+      </Typography>
+    </Container>
+  );
+}
